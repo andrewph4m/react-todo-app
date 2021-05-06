163238c4dba3b5bb640fe531559f51d7cafac0f1
Andrew Pham <andrew.pham@hobbywarehouse.com.au>
2021-05-06 13:38:23 +1000

    Added data and headers

---
+++ src/components/TodosList.js
@@ -0,0 +1,14 @@
+import TodoItem from "./TodoItem";
+import React from "react"
+class TodosList extends React.Component {
+    render() {
+        return (
+            <ul>
+                {this.props.todos.map(todo => (
+                    <TodoItem key={todo.id} todo={todo} />
+                ))}
+            </ul>
+        )
+    }
+}
+export default TodosList
